80fb099342c4d0db209170cff6192ce415445732
Felipe Tavares <felipe.tavares@outlook.com>
2024-10-09 18:37:51 -0300

    Corrigindo corrupção no código

---
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("Felipe", "Tavares", "123456", "email@email.com");
+        student.AddSubscription(subscription);
+    }
+}
